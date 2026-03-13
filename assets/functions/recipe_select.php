c69104f69128eefb148b6f67ad234f618f0ba0fa
Juline Thorgren <juline.thorgren0016@stud.hkr.se>
2026-03-13 15:04:31 +0000

    Lagt till filtrering för recept

--- assets/functions/recipe_select.php
+++ assets/functions/recipe_select.php
@@ -1,11 +1,36 @@
 <?php
-// Gets  information from recipes and  users tables
+// Gets information from recipes and users tables
 $sql = "SELECT recipes.*, users.userID, users.userPicture, users.userFirstname, users.userSurname
         FROM recipes 
-        JOIN users ON recipes.userID = users.userID 
-        ORDER BY recipeDate DESC";
+        JOIN users ON recipes.userID = users.userID
+        WHERE 1=1";
+
+$params = [];
+
+// Filter: cuisine
+if (!empty($_GET['cuisine'])) {
+    $sql .= " AND recipes.recipeCuisine = ?";
+    $params[] = $_GET['cuisine'];
+}
+
+// Filter: protein
+if (!empty($_GET['protein'])) {
+    $sql .= " AND recipes.recipeProtein = ?";
+    $params[] = $_GET['protein'];
+}
+
+// Filter: difficulty
+if (!empty($_GET['difficulty'])) {
+    $sql .= " AND recipes.recipeDifficulty = ?";
+    $params[] = $_GET['difficulty'];
+}
+
+// Sorts recipes by latest date
+$sql .= " ORDER BY recipeDate DESC";
+
 // Prepares a query
 $stmt = $dbh->prepare($sql);
+
 // Sends query to database
-$stmt->execute();
+$stmt->execute($params);
 ?>
